ae1946a505633c147050552eadd25b8a99c05d51
Olli Jokinen <olli.jokinen@cs.helsinki.fi>
2012-06-29 02:44:32 +0300

    numzoomlevels 17 -> 16

--- js/map.js
+++ js/map.js
@@ -71,7 +71,7 @@
   baseLayer = new OpenLayers.Layer.OSM("MapQuest", ["http://otile1.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.png", "http://otile2.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.png", "http://otile3.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.png", "http://otile4.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.png"], {
     transitionEffect: 'resize',
     attribution: "Data, imagery and map information provided by <a href='http://www.mapquest.com/'  target='_blank'>MapQuest</a>, <a href='http://www.openstreetmap.org/' target='_blank'>Open Street Map</a> and contributors, <a href='http://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC-BY-SA</a>  <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
-    numZoomLevels: 17
+    numZoomLevels: 16
   });
 
   map = new OpenLayers.Map('map', {
